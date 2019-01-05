32bed1dffb3f4f27bf2c6319041f9c1d5d8232ec
Sanjeev Jikamade <sanjeev.jikamade@gmail.com>
2019-01-06 02:28:15 +0530

    Added Email password validation

--- app/src/main/java/com/sanjeev/androidtest/MainActivity.java
+++ app/src/main/java/com/sanjeev/androidtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sanjeev.androidtest;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sanjeev.androidtest.Interface.LoginResultCallbacks;
+import com.sanjeev.androidtest.ViewModel.LoginViewModel;
+import com.sanjeev.androidtest.ViewModel.LoginViewModelFactory;
+import com.sanjeev.androidtest.databinding.ActivityMainBinding;
+
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity implements LoginResultCallbacks {
 
@@ -16,15 +24,18 @@ public class MainActivity extends AppCompatActivity implements LoginResultCallba
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(ViewModelProviders.of(this, new LoginViewModelFactory(this))
+                .get(LoginViewModel.class));
     }
 
     @Override
     public void onSuccess(String result) {
-
+        Toasty.success(this, "Login Success", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onError(String result) {
-
+        Toasty.error(this, "Login Error!!!", Toast.LENGTH_LONG).show();
     }
 }
