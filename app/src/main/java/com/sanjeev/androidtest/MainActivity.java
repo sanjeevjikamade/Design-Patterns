0dbed18a0444589b45b1e193dd010267674d5ee8
Sanjeev Jikamade <sanjeev.jikamade@gmail.com>
2019-01-06 02:03:10 +0530

    Added text watcher to layout

--- app/src/main/java/com/sanjeev/androidtest/MainActivity.java
+++ app/src/main/java/com/sanjeev/androidtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sanjeev.androidtest;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +8,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.sanjeev.androidtest.Interface.LoginResultCallbacks;
+
+public class MainActivity extends AppCompatActivity implements LoginResultCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onSuccess(String result) {
+
+    }
+
+    @Override
+    public void onError(String result) {
+
     }
 }
